8d6fd9c973eb78aa0b62f373b05834f708bc052c
ANDREY <41635263+FreylGit@users.noreply.github.com>
2022-10-18 14:18:47 +0300

    Настроил базу данных

---
+++ MarketplaceCF/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketplaceCF.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+                
+        }
+    }
+}
